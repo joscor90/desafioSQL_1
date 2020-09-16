eb13723f48e6d412fee9dbeb9074b41837c9fe6b
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-09-16 12:04:10 -0300

    Added column titulo to post table

--- desafio1.sql
+++ desafio1.sql
@@ -6,3 +6,6 @@ CREATE TABLE post(id SERIAL, nombre_usuario VARCHAR(15), fecha_creacion DATE DEF
 
 -- Insertar 3 post, 2 para el usuario "Pamela" y uno para "Carlos"
 INSERT INTO post(nombre_usuario, contenido, descripcion) VALUES ('Pamela', 'lorem ipsum...', 'lorem ipsum...'), ('Pamela', 'lorem ipsum...', 'lorem ipsum...'), ('Carlos', 'lorem ipsum...', 'lorem ipsum...');
+
+-- Modificar la tabla post, agregando la columna título
+ALTER TABLE post ADD COLUMN titulo VARCHAR(20);
