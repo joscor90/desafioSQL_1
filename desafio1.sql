83e6c8f85d01dbf24b52ea383749918372e0be51
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-09-16 12:02:49 -0300

    Added 3 posts to post table

--- desafio1.sql
+++ desafio1.sql
@@ -3,3 +3,6 @@ CREATE DATABASE posts;
 
 -- Crear tabla post, con los atributos id, nombre de usuario, fecha de creación, contenido, descripción
 CREATE TABLE post(id SERIAL, nombre_usuario VARCHAR(15), fecha_creacion DATE DEFAULT NOW(), contenido VARCHAR(200), descripcion VARCHAR(150), PRIMARY KEY (id));
+
+-- Insertar 3 post, 2 para el usuario "Pamela" y uno para "Carlos"
+INSERT INTO post(nombre_usuario, contenido, descripcion) VALUES ('Pamela', 'lorem ipsum...', 'lorem ipsum...'), ('Pamela', 'lorem ipsum...', 'lorem ipsum...'), ('Carlos', 'lorem ipsum...', 'lorem ipsum...');
